722bc7436672b1bba587eb3ce1510cbb67436e27
Dallin Christensen <christensen.dallin@gmail.com>
2020-09-29 07:28:20 -0700

    learning junk

---
+++ dang-thats-delicious/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController')
+const { catchErrors } = require('../handlers/errorHandlers')
+
+// Do work here
+router.get('/', storeController.homepage);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
+
+module.exports = router;
