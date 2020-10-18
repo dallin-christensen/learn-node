f51c77ad943084a8b09fc5f4dd8f61ff7adf4534
Dallin Christensen <christensen.dallin@gmail.com>
2020-10-18 13:06:05 -0700

    tags query & page

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -85,3 +85,16 @@ exports.getStoreBySlug = async (req, res, next) => {
   }
   res.render('store', { store, title: store.name })
 }
+
+exports.getStoresByTag = async (req, res, next) => {
+  const tag = req.params.tag
+  const tagQuery = tag || { '$exists': true }
+
+  const tagsPromise = Store.getTagsList()
+  const storesPromise = Store.find({ tags: tagQuery })
+
+  const [tags, stores] = await Promise.all([ tagsPromise, storesPromise ])
+
+
+  res.render('tags', { tags, stores, tag, title: 'Tags' })
+}
--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -35,4 +35,12 @@ storeSchema.pre('save', async function(next) {
   // TODO make sure slugs unique
 })
 
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ])
+}
+
 module.exports = mongoose.model('Store', storeSchema)
